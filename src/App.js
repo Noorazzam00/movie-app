1b91cfda8e8056e37d5e37e0e2e36ad8f81c7e18
NoorAzzam <noorazzam19@gmail.com>
2023-08-07 16:43:11 +0300

    adding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Movies from "./component/list"
+import {Route, Routes} from 'react-router-dom'
+import MovieCard from "./component/moviecards";
+import DesCard from "./component/DesCard";
+
 
 function App() {
   // console.log("object");
   return (
-    <>
-      <div>
-        <Movies />
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<MovieCard/>}/>
+      <Route path=":id" element={<DesCardd/>}/>
+    </Routes>
   );
 }
 
